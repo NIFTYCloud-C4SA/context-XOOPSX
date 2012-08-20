6a29a0ffdbdecc26063fa1e391f92f4c31eb6469
OkawaUki <okawa.yuki@nifty.co.jp>
2012-08-20 11:03:25 +0900

    update makecontext.sh

--- makecontext.sh
+++ makecontext.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+﻿#!/bin/bash
 
 ## XOOPS Xコンテクスト作成シェルスクリプト
 
@@ -8,16 +8,17 @@ BIN_RM="/bin/rm -rf"
 BIN_TAR="/usr/bin/tar"
 BIN_CD="cd"
 
-URL_ARCHIVE="http://sourceforge.jp/projects/xoopscube22x/downloads/55285/xoopsx-103-2.tar.gz/"
+#URL_ARCHIVE="http://sourceforge.jp/projects/xoopscube22x/downloads/55285/xoopsx-103-2.tar.gz"
+URL_ARCHIVE="http://sourceforge.jp/frs/redir.php?m=osdn&f=%2Fxoopscube22x%2F55285%2Fxoopsx-103-2.tar.gz"
 
 ## make context
-
 ${BIN_CD} ./contexts/krm/
-${BIN_CURL} -o xoopsx.tar.gz ${URL_ARCHIVE}
-${BIN_TAR} zxf xoopsx.tar.gz
+${BIN_CURL} -o ./xoopsx.tar.gz ${URL_ARCHIVE}
+${BIN_TAR} zxvf ./xoopsx.tar.gz
 ${BIN_MV} html public_html
 ${BIN_MV} public_html/install/class/settingmanager.php public_html/install/class/settingmanager_org.php
 ${BIN_MV} settingmanager.php public_html/install/class/settingmanager.php
+${BIN_RM} xoopsx.tar.gz
 
 ## create archive
 ${BIN_TAR} czpf ./web_root.tar.gz {public_html,xoops_trust_path,docs,extras}
